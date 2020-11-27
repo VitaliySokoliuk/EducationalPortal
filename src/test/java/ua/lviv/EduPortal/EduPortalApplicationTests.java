fb6b4840386d726dbaf1db0666d1836340210cfc
Vitaliy Sokoliuk <Vit.sokol0@gmail.com>
2020-11-27 13:17:32 +0200

    initial commit

---
+++ src/test/java/ua/lviv/EduPortal/EduPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lviv.EduPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
